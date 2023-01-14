d730310a1e7a62b1f2b92c6821bd0dd47f15268a
developer@thermoacoustics.uk <developer@thermoacoustics.uk>
2023-01-14 22:34:42 +0000

    on off basic code

---
+++ onoff.js
@@ -0,0 +1,26 @@
+const noble = require('@abandonware/noble');
+
+noble.on('stateChange', async (state) => {
+  console.log(state);
+  if (state === 'poweredOn') {
+    console.log('Start scanning...');
+    await noble.startScanningAsync();
+  }
+});
+
+var deviceId = 'A105000002BA'
+var deviceId = deviceId.toLowerCase();
+var device = undefined
+
+noble.on('discover', async (peripheral) => {
+  var periId = peripheral.id.toLowerCase();
+  console.log(periId,periId==deviceId,peripheral.advertisement.localName);
+  if (periId == deviceId) {
+    await noble.stopScanningAsync();
+    console.log('Device found.');
+    console.log(peripheral);
+    await peripheral.connectAsync();
+    peripheral.writeHandle(0x0009, new Buffer([0xcc,0x23,0x33]));
+  };
+});
+
